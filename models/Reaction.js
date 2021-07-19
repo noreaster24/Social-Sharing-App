c9d889d17f379ec0538a07a92e17ffaf73f5496c
Joe Porrazzo <joepa2624@me.com>
2021-07-18 17:06:18 -0700

    fixed export bug

---
+++ models/Reaction.js
@@ -0,0 +1,32 @@
+// const { Schema, model, Types } = require('mongoose');
+// const dateFormat = require('../utils/dateFormat');
+
+// const reactionSchema = new Schema(
+//     {
+//         reactionId: {
+//             type: Schema.Types.ObjectId,
+//             default: () => new Types.ObjectId()
+//         },
+//         reactionBody: {
+//             type: String,
+//             required: true,
+//             max: 280
+//         },
+//         username: {
+//             type: String,
+//             required: true
+//         },
+//         createdAt: {
+//             type: Date,
+//             default: Date.now,
+//             get: createdAtVal => dateFormat(createdAtVal)
+//         }
+//     },
+//     {
+//         toJSON: {
+//             getters: true
+//         }
+//     }
+// );
+
+// module.exports = Reaction
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(require('./routes'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-sharing-app', {
